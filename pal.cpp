57803cebf85344e6cc3f4cfa2e77f9c788981160
Jeremy Gresham <greshjs@gmail.com>
2017-09-21 12:36:05 -0400

    factored out printing

--- pal.cpp
+++ pal.cpp
@@ -1,33 +1,5 @@
-#include "lang.hpp"
-#include <sstream>
-
+#include "print_lang.hpp"
 using namespace lang;
-
-auto print(const std::string& s, std::shared_ptr<node_t>& x)
-{
-  for(auto& x : s)
-  {
-    std::cout << "D_{" << x << "} ";
-  }
-  std::cout << "L" << "\n";
-  std::cout << "nullable?: " << x->null() << "\n\n";
-}
-
-
-
-auto run(const std::initializer_list<std::pair<int, std::string>>& lst,
-         std::shared_ptr<node_t>& lang)
-         -> void
-{
-  std::stringstream ss;
-  auto x = lang;
-  for(auto& c : lst)
-  {
-    x = x->deriv(c);
-    ss << c.first;
-    print(ss.str(), x);
-  }
-}
 
 
 int main(int argc, char* argv[])
